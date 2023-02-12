fe6232e5c64ccd8131f38ff540d0f5248b634729
DT0998 <duongkytrieuvn@gmail.com>
2023-02-12 21:41:47 +0700

    feat: add smth

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
 // Code goes here!
+const userName = "Max";
+// userName = 'Maximilian';
+
+let age = 30;
+age = 29;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// if (age > 20) {
+//   var isOld = true;
+// }
+
+// console.log(isOld);
+
+// console.log(result);
